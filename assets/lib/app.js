dc2df3407566cde0f68e9ff577ea6e0d63557cf3
Tom Clarke <tomc@designyourcode.co.uk>
2016-01-25 14:28:38 +0000

    Adding favicon, ga and adjusting styling

--- assets/lib/app.js
+++ assets/lib/app.js
@@ -1,37 +1,47 @@
-var app = angular.module('randomizerApp', []);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-app.controller('ListController', function ($scope, $http, $timeout) {
+ga('create', 'UA-33321634-1', 'auto');
+ga('send', 'pageview');
 
-	$scope.list = [];
-	$scope.show = false;
+var app = angular.module('randomizerApp', ['ngSanitize']);
 
-	$scope.addToList = function () {
-		$scope.list.push($scope.formListText);
+app.controller('ListController', ['$scope', '$http', '$timeout',
+	function ($scope, $http, $timeout) {
 
-		$scope.formListText = '';
-	};
-
-	$scope.deleteSkill = function(index) {	
-		$scope.list.splice(index, 1);
-	};
-  
-	$scope.getResult = function () {
-		$scope.show = true;
-		$scope.hideloader = false;
-		$scope.result = '';
-
-		$timeout(function(){
-			$scope.hideloader = true;
-			$scope.result = $scope.list[Math.floor(Math.random() * $scope.list.length)]
-		}, 1500);
-	};
-
-	$scope.reset = function () {
-		$scope.formListText = '';
-		$scope.result = '';
 		$scope.list = [];
 		$scope.show = false;
-		$scope.hideloader = false;
-	};
 
-});
+		$scope.addToList = function () {
+			$scope.list.push($scope.formListText);
+
+			$scope.formListText = '';
+		};
+
+		$scope.deleteSkill = function(index) {	
+			$scope.list.splice(index, 1);
+		};
+	  
+		$scope.getResult = function () {
+			$scope.show = true;
+			$scope.hideloader = false;
+			$scope.result = '';
+
+			$timeout(function(){
+				$scope.hideloader = true;
+				$scope.result = '<p class="large">' + $scope.list[Math.floor(Math.random() * $scope.list.length)] + '</p>'
+			}, 1500);
+		};
+
+		$scope.reset = function () {
+			$scope.formListText = '';
+			$scope.result = '';
+			$scope.list = [];
+			$scope.show = false;
+			$scope.hideloader = false;
+		};
+	
+	}
+]);
